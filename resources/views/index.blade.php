52c518ca8390eee57bd0ffb526abf1abaa5eb8e6
carenrita <d00262189@student.dkit.ie>
2024-03-12 12:21:16 +0000

    intial changes: main page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
                 <a 
                     href="/blog"
                     class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
-                    Read More
+                    book a table 
                 </a>
             </div>
         </div>
